7a225b3e4574b38705ef10f65d277ca28b657748
molinaro karine <molinaro.karine@hotmail.com>
2022-06-11 09:00:29 +0200

    Class Aticle

--- JS/Article.js
+++ JS/Article.js
@@ -1,46 +1,91 @@
-export let showRecipe = [] // les recettes à affichées
+//  classe pour une recette
+class Recipe {
+  constructor (name, time, description){
+      this.name = name;
+      this.time = time;
+      this.description = description;
+      this.ingredients = [];
+      this.appliances = [];
+      this.utensils = [];
+  }
+  addIngredient(ingredient){
+      this.ingredients.push(ingredient)
+  }
+  addAppliance(appliance){
+      this.appliances.push(appliance)
+  }
+  addUtensil(utensil){
+      this.utensils.push(utensil)
+  }
+ // Méthode pour créer des fiches de recettes html
+  createRecipeCard(){
+      const recipeCard = document.createElement('a');
+      recipeCard.setAttribute('href', '#');
+      
+      const article = document.createElement('article');
+      article.classList.add('card');
+      
+      const cardImage = document.createElement('div');
+      cardImage.classList.add('card-image');
+      
+      const cardBody = document.createElement('div');
+      cardBody.classList.add('card-body');
+      
+      const cardInfo = document.createElement('div');
+      cardInfo.classList.add('card-info');
+      
+      const h2 = document.createElement('h2');
+      h2.classList.add('card-title');
+      h2.textContent=this.name;
+      
+      const icon = document.createElement('img');
+      icon.classList.add('time-icon');
+      icon.setAttribute('src', "./public/img/time.svg");
+      icon.setAttribute('alt', "");
+      
+      const cardTime = document.createElement('p');
+      cardTime.classList.add('card-time','card-text');
+      cardTime.textContent=`${this.time} min`;
+      
+      const  cardDescription= document.createElement('div');
+      cardDescription.classList.add('card-description');
+      
+      const cardIngredients = document.createElement('ul');
+      cardIngredients.classList.add('card-ingredients','card-text');
+      // créer une liste d'ingrédients et l'ajouter à l'ul
+      this.ingredients.forEach(item => {
+          const li = document.createElement('li');
+          li.classList.add('card-ingredients-item');
+          const span = document.createElement('span');
+          span.classList.add('bold');
+          span.textContent=item.name + ': ';
+          li.appendChild(span);
+          // si la quantité existe, affichez-la
+          if (item.quantity) {
+              const unit = item.unit == 'grammes'? 'g': item.unit.toLowerCase() == 'litres'? 'L': item.unit // const avec condition: si = grammes, remplacé par 'g' etc.. sinon garder l'unité telle quelle
+              const quantity = document.createTextNode(item.quantity + unit); // ici unit est la const et non pas item.unit
+              li.appendChild(quantity);
+          } 
+          cardIngredients.appendChild(li);
+      })
 
-/*affichage de la recette */
-export const Article = () => {
-    const main = document.querySelector('.section-cards')
-    main.replaceChildren()
-    showRecipe[0].forEach((recipe) => {// pour chaque recette 
-        const newArcticle = document.createElement('article')
-        newArcticle.classList.add("col", "section-cards", "mt-0", "mb-5")
-        
-        recipe.ingredients.map(e => {// pour que les unités correspondant à la maquette
-            if (e.unit === "grammes") e.unit = "g"
-            else if (e.unit === "cuillères à soupe") e.unit = "cuillères"
-        })
-        const newHtml = `
-    <div class="card h-100  bg-opacity-10 border-0">
-      <img class ="recipe-img" src ="./public/img/genericImg.svg" alt = "">
-      <div class="recipe card-body">
-        <div class="recipe-head d-flex justify-content-between mb-3">
-          <div class="card-title">${recipe.name}</div>
-            <div class="card-text text-small">
-              <i class="far fa-clock"></i><strong> ${recipe.time} min</strong>
-            </div>
-          </div>
-          <div class="d-flex flex-row">
-          <div class="col-6 ingredients">
-            <ul class="card-text text-small mb-0">${recipe.ingredients.map(e => {
-              return `<li class = " ingredients ${e}" title="${e}" ><h4>${e.ingredient}</h4><span> ${e.quantity ? ' :' : ''} ${e.quantity ? e.quantity : ' '} ${e.unit ? e.unit : ' '} </span> </li>`
-              }).join("")}
-            </ul>
-          </div>
-          <p class="col-6 card-text text-small">${recipe.description}</p>
-        </div>
-      </div>
-    </div>`;
-        newArcticle.innerHTML = newHtml
-        main.appendChild(newArcticle)// ajout de la nouvelle recette au main
-    })
-    if (showRecipe[0].length === 0) {//si aucune recette, un message est affiché
-        const newArcticle = document.createElement('article')
-        const newHtml = `<p class ="no-result">Aucune recette ne correspond à votre critère de recherche.</p>`
-        newArcticle.innerHTML = newHtml
-        main.appendChild(newArcticle)// ajout de la nouvelle recette au main
-    }
+      const cardInstructions = document.createElement('p');
+      cardInstructions.classList.add('card-instructions','card-text');
+      cardInstructions.textContent=this.description.substr(0, 181) + ' ...';
 
-}
+      cardDescription.appendChild(cardIngredients);
+      cardDescription.appendChild(cardInstructions);
+
+      cardInfo.appendChild(h2);
+      cardInfo.appendChild(icon);
+      cardInfo.appendChild(cardTime);
+      
+      cardBody.appendChild(cardInfo);
+      cardBody.appendChild(cardDescription);
+
+      article.appendChild(cardImage);
+      article.appendChild(cardBody);
+      recipeCard.appendChild(article);
+      return recipeCard
+  }
+} 
